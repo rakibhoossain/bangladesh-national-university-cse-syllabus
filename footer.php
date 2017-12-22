a8c4abe56e9d2d0bce45a67a97f0b5855fd379fb
Rakib Hossain <serakib@gmail.com>
2017-12-22 17:56:45 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,13 +4,17 @@
 <!-- <div id="notification"><span class="msg">notification</span></div> -->
 <footer>
 	<div class="copyright text-center">
-		Design &amp; Develop by <a href="http://facebook.com/prof.rakib">Rakib Hossain</a>
-	</div>
+		<span class="cpyright in-flex">Design &amp; Develop by </span>
+		<span class="cpyname  in-flex"><a href="http://rakibhossain.cf" target="_blank">Rakib Hossain</a></span>
+		<span class="social  in-flex">
+			<a href="http://www.github.com/serakib" target="_blank"><i class="fa fa-github">&nbsp;</i> </a>
+        	<a href="http://www.linkedin.com/in/serakib" target="_blank"><i class="fa fa-linkedin">&nbsp;</i></a>
+        	<a href="http://www.twitter.com/serakib" target="_blank"><i class="fa fa-twitter">&nbsp;</i></a>
+        	<a href="http://www.facebook.com/prof.rakib" target="_blank"><i class="fa fa-facebook">&nbsp;</i></a>
+		</span>
+    </div>
 </footer>
 </body>
 
 <script src="assets/main.js"></script>
-
-
-
 </html>
